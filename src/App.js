debe03b77b0cc5dc6464401beeef658e857a3945
Siju Xavier <siju.xavier@ucalgary.ca>
2026-03-06 11:47:58 -0700

    l

--- src/App.js
+++ src/App.js
@@ -2,24 +2,22 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const currentYear = new Date();
+  const isLoggedIn = true;
+  function MyComponent({ isLoggedIn }) {
+    return isLoggedIn ? <p>Welcome, user!</p> : <p>Please log in.</p>;
+  }
+
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>ENSF-381: Full Stack Web Development</h1>
+      <p>React Components</p>
+      <p>{`${currentYear.getFullYear()}`}</p>
+      <MyComponent isLoggedIn={isLoggedIn} />
+
     </div>
-  );
+  )
 }
 
 export default App;
